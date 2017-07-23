1b681691e19b9abd5b49616eb0a83f42d92b6bb6
Anuj Goyal <anuj.goyal@gmail.com>
2017-07-22 22:38:38 -0700

    concat strings

--- s.go
+++ s.go
@@ -11,50 +11,51 @@ import (
 var wg sync.WaitGroup
 
 func getStock(s string) {
-	//for i := 1; i <= 12; i++ {
-        fmt.Printf("%s \n", s)
-        // time.Sleep(100 * time.Millisecond)
-	wg.Done()
+	var u1 string = "http://download.finance.yahoo.com/d/quotes.csv?s="
+	var u2 string = "&f=srp5p6m3m4m6m8j1j4y"
+        var url string = u1 + s + u2
+        fmt.Printf("url: %s \n", url)
+	//wg.Done()
 }
 
 // https://stackoverflow.com/questions/8757389/reading-file-line-by-line-in-go
 func readFile() []string {
-        // open file
+	// open file
 	file, err := os.Open("stocklist.txt")
-	if err != nil { log.Fatal(err) }
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer file.Close()
 
-        // setup slice, https://blog.golang.org/go-slices-usage-and-internals
-        var sl []string
-        sl = make([]string, 0)
+	// setup slice, https://blog.golang.org/go-slices-usage-and-internals
+	var sl []string
+	sl = make([]string, 0)
 
-        // read file line by line
+	// read file line by line
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-                sl = append(sl, scanner.Text())
+		sl = append(sl, scanner.Text())
 		// fmt.Println(sl);
 	}
 
-        // more error checking
+	// more error checking
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-        return sl
+	return sl
 }
 
 func main() {
 	// slice of ticker symbols
-        var sl []string
-        sl = readFile()
-        //fmt.Printf("sl(%d): %s\n", len(sl), sl)
-        for i, s := range sl {
-            fmt.Printf("sl[%d] %s\n", i, s)
-        }
+	var sl []string
+	sl = readFile()
+	fmt.Printf("sl(%d): %s\n", len(sl), sl)
+	//for i, s := range sl { fmt.Printf("sl[%d] %s\n", i, s) }
 
+        getStock("AAPL")
 	/*for n := 2; n <= 12; n++ {
 		wg.Add(1)
 		go getStock(n)
 	}
 	wg.Wait()*/
 }
-
