3bdc4177a8ede77bba30ba487792dbf73e8b8b1c
Anuj Goyal <anuj.goyal@gmail.com>
2017-07-26 00:42:02 -0700

    channel and goroutines

--- s1.go
+++ s1.go
@@ -1,48 +1,52 @@
 package main
+
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "io/ioutil"
-    "time"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
 )
 
 // https://www.youtube.com/watch?v=f6kdp27TYZs (15m)
 // Generator: function that returns a channel
-func getStocks(sl []string) <- chan string {
-        c := make (chan string)
+func getStocks(sl []string) <-chan string {
+	c := make(chan string)
 	for _, s := range sl {
-		go getStock(s,c)
+		go getStock(s, c)
 	}
-        return c
+	return c
 }
 
 func getStock(s string, c chan string) {
-        resp, err := http.Get("http://goanuj.freeshell.org/go/" + s + ".txt")
+	resp, err := http.Get("http://goanuj.freeshell.org/go/" + s + ".txt")
 	if err != nil {
-            log.Printf(s + ": "+ err.Error())
-            c <- err.Error() // channel
-            return
-        }
-	body, _:= ioutil.ReadAll(resp.Body)
-        resp.Body.Close() // close ASAP to prevent too many open file desriptors
-        val := string(body)
-        //fmt.Printf("body: %s", val)
+		log.Printf(s + ": " + err.Error())
+		c <- err.Error() // channel
+		return
+	}
+	body, _ := ioutil.ReadAll(resp.Body)
+	resp.Body.Close() // close ASAP to prevent too many open file desriptors
+	val := string(body)
+	//fmt.Printf("body: %s", val)
 	c <- val
-        return
+	return
 }
 
 func main() {
-        start := time.Now()
-	var sl = []string{"AAPL","AMZN","GOOG","FB","NFLX"}
-        // AG: remember to add for loop increasing size of sl to 1280
+	start := time.Now()
+	var sl = []string{"AAPL", "AMZN", "GOOG", "FB", "NFLX"}
+	// creates slice of 1280 elements
+	for i := 0; i < 3; i++ {
+		sl = append(sl, sl...)
+	}
+	fmt.Printf("sl(size): %d\n", len(sl))
 
-        // get channel that returns only strings
-        c := getStocks(sl)
-        for i := 0 ; i < len(sl); i++ {
-            fmt.Printf("%s", <-c)
-        }
+	// get channel that returns only strings
+	c := getStocks(sl)
+	for i := 0; i < len(sl); i++ {
+		fmt.Printf("%s", <-c)
+	}
 
-        fmt.Printf("main: %.2fs elapsed.\n", time.Since(start).Seconds())
+	fmt.Printf("main: %.2fs elapsed.\n", time.Since(start).Seconds())
 }
-
