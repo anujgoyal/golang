bc7de261f6f7475b22aae57da46fb5ceb2fb7d6d
Anuj Goyal <anuj.goyal@gmail.com>
2017-07-25 23:06:35 -0700

    use lonestar; return if error

---
+++ s1.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "sync"
+    "net/http"
+    "io/ioutil"
+    "time"
+)
+
+var wg sync.WaitGroup
+
+func getStock(s string) {
+        resp, err := http.Get("http://goanuj.freeshell.org/go/" + s + ".txt")
+	if err != nil {
+            log.Printf(s + ": "+ err.Error())
+            wg.Done()
+            return
+        }
+	body, _:= ioutil.ReadAll(resp.Body)
+        resp.Body.Close() // close ASAP to prevent too many open file desriptors
+        val := string(body)
+        fmt.Printf("body: %s", val)
+	wg.Done()
+}
+
+func main() {
+        start := time.Now()
+	var sl = []string{"AAPL","AMZN","GOOG","FB","NFLX"}
+	for _, s := range sl {
+		wg.Add(1)
+		go getStock(s)
+	}
+	wg.Wait()
+        fmt.Printf("main: %.2fs elapsed.\n", time.Since(start).Seconds())
+}
+
