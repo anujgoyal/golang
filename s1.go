d81fa18bf51ae4a708278e0c43c0e82e615a611e
Anuj Goyal <anuj.goyal@gmail.com>
2017-07-27 16:25:04 -0700

    increase slice elements

--- s1.go
+++ s1.go
@@ -22,14 +22,14 @@ func getStock(s string, c chan string) {
 	resp, err := http.Get("http://goanuj.freeshell.org/go/" + s + ".txt")
 	if err != nil {
 		log.Printf(s + ": " + err.Error())
-		c <- err.Error() // channel
+		c <- err.Error() // channel send
 		return
 	}
 	body, _ := ioutil.ReadAll(resp.Body)
 	resp.Body.Close() // close ASAP to prevent too many open file desriptors
 	val := string(body)
 	//fmt.Printf("body: %s", val)
-	c <- val
+	c <- val // channel send
 	return
 }
 
@@ -37,7 +37,7 @@ func main() {
 	start := time.Now()
 	var sl = []string{"AAPL", "AMZN", "GOOG", "FB", "NFLX"}
 	// creates slice of 1280 elements
-	for i := 0; i < 3; i++ {
+	for i := 0; i < 8; i++ {
 		sl = append(sl, sl...)
 	}
 	fmt.Printf("sl(size): %d\n", len(sl))
@@ -45,7 +45,7 @@ func main() {
 	// get channel that returns only strings
 	c := getStocks(sl)
 	for i := 0; i < len(sl); i++ {
-		fmt.Printf("%s", <-c)
+		fmt.Printf("%s", <-c) // channel recv
 	}
 
 	fmt.Printf("main: %.2fs elapsed.\n", time.Since(start).Seconds())
