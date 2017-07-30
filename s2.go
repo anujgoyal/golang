e235fb6cbc08a0a9a47db765de64dca1fcbbaa7e
Anuj Goyal <anuj.goyal@gmail.com>
2017-07-30 09:57:06 -0700

    still trying to make the HTTP client queue instead of me doing it

--- s2.go
+++ s2.go
@@ -13,24 +13,33 @@ import (
         "time"
 )
 
+// using init doesn't seem to be helping
+var netClient = &http.Client{}
+
 func init() {
         fmt.Println("init in s2.go")
         // setup Client
 	tr := &http.Transport{
-		IdleConnTimeout:  time.Second*1,
-		//MaxIdleConns:       10,
+		//IdleConnTimeout:  time.Second*5,
+		MaxIdleConns:       20,
+                MaxIdleConnsPerHost:  20,
 		//dDisableCompression: true,
 	}
-	client := &http.Client{Transport: tr}
+	netClient = &http.Client{Transport: tr}
+
+        // set connection timeout
+        timeout := time.Duration(1 * time.Second)
+        client := http.Client{
+            Timeout: timeout,
+        }
         _ = client
 }
 
 func main() {
 	// read file, and output slice of ticker symbols
 	var sl []string
-	sl = readFile("wilshire.txt")
 	sl = readFile("stocklist.txt")
-
+	sl = readFile("wilshire.txt")
 
 	// create channel, create slice
 	var c chan string
@@ -39,30 +48,27 @@ func main() {
 	for _, s := range sl {
 		go getStock(s, c)
 	}
-	// create slice
-	info := make([]string, 0)
+
+	info := make([]string, 0) // create slice
 	// get info from channel
 	for i := 0; i < len(sl); i++ {
 		info = append(info, <-c)
 	}
-	fmt.Printf("\ninfo: %d\n", len(info))
-	fmt.Printf("info: %s\n", info)
-	fmt.Println("main done.")
+
+        // print stats
+	fmt.Printf("\n\ninfo(%d):\n%s\nmain done.\n", len(info), info)
 }
 
 func getStock(s string, c chan string) {
-        // set connection timeout
-        timeout := time.Duration(1 * time.Second)
-        client := http.Client{
-            Timeout: timeout,
-        }
         // HTTP get stock symbol
 	fmt.Printf("%s ", s)
-	resp, err := client.Get("http://download.finance.yahoo.com/d/quotes.csv?s=" + s + "&f=srp5p6m3m4m6m8j1j4y")
+	resp, err := http.Get("http://download.finance.yahoo.com/d/quotes.csv?s=" + s + "&f=srp5p6m3m4m6m8j1j4y")
         // resp, err := http.Get("http://download.finance.yahoo.com/d/quotes.csv?s=" + s + "&f=srp5p6m3m4m6m8j1j4y")
 	if err != nil {
 		//log.Fatalf("http.Get => %v", err.Error())
 		log.Printf("http.Get => %v", err.Error())
+		c <- err.Error()
+                return
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body) // read body
@@ -70,6 +76,7 @@ func getStock(s string, c chan string) {
 		log.Fatalf("ioutil.ReadAll => %v", err.Error())
 	}
 	c <- string(body) // https://stackoverflow.com/questions/38673673/access-http-response-as-string-in-go
+        return
 }
 
 // https://stackoverflow.com/questions/8757389/reading-file-line-by-line-in-go
