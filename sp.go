5f19f5fb34455c0bbe59fd971b0f7e407649b29b
Anuj Goyal <anuj.goyal@gmail.com>
2017-07-30 13:12:18 -0700

    discovered subtle bug when jobs > channel queue length, results in deadlock because the worker for loop never makes forward progress

--- sp.go
+++ sp.go
@@ -9,54 +9,62 @@ import "io/ioutil"
 
 // The worker will run several concurrent instances. These workers will receive
 // work on the `jobs` channel and send the corresponding results on `results`.
-// Sleep 1 second per job to simulate an expensive task.
 func worker(id int, jobs <-chan string, results chan<- string) {
 	for j := range jobs {
-		fmt.Println("worker", id, "started  job", j)
-		//time.Sleep(time.Second)
+		// fmt.Println("worker", id, "started  job", j)
+		// the expensive task
 		resp, err := http.Get("http://goanuj.freeshell.org/go/" + j + ".txt")
 		if err != nil {
 			log.Printf(j + ": " + err.Error())
-			fmt.Println("worker", id, "finished job", j)
+			//fmt.Println("worker", id, "finished job", j)
 			results <- err.Error() // channel send
 		} else {
 			body, _ := ioutil.ReadAll(resp.Body)
 			resp.Body.Close() // close ASAP to prevent too many open file desriptors
 			val := string(body)
-			fmt.Println("worker", id, "finished job", j)
+			//fmt.Println("worker", id, "finished job", j)
 			results <- val // channel send
 		}
 	}
 }
 
 func main() {
-	// to use `worker pool, need to send work and then collect their results.
-	// 2 channels are needed for this
-	jobs := make(chan string, 500)
-	results := make(chan string, 500) // AG will need to increase this!
+	// to use `worker pool, need to send work and then collect the results.
+	// 2 channels are needed todo this
+	// AG: heuristic, channel queue should be approx. the number of stocks
+	// NOTE: if jobs queue isn't large enough then a deadlock will occur, this is very subtle
+	// basically the jobs channel is blocking because the queue is too small and the code to
+	// close the `jobs channel doesn't get executed, and neither does the `results channel for for loop
+	jobs := make(chan string, 8000)
+	results := make(chan string, 8000) // AG will need to increase this!
 
-	// Startup N workers, initially blocked because there are no jobs yet.
-	for w := 1; w <= 40; w++ {
+	// startup N workers, initially blocked because there are no jobs yet.
+	// work network: highest is ~2000 workers,
+	// home network: highest is ~250 workers, ... but why?
+	for w := 1; w <= 230; w++ {
 		go worker(w, jobs, results)
 	}
 
 	var sl = []string{"AAPL", "AMZN", "GOOG", "FB", "NFLX"}
-	// slice append to repeat stocks; 5 for 160, 6 for 320, 7 for 640
-	for i := 0; i < 7; i++ {
+	// slice append to repeat stocks
+	// 5 for 160, 6 for 320, 7 for 640, 8 for 1280, 9 for 2540, 10 for 5080
+	for i := 0; i < 10; i++ {
 		sl = append(sl, sl...)
 	}
-	// Send N stocks to `jobs channel and then close `jobs channel to signify no more work
-	// will cause the for loop in `worker to drop out
+	// send N stocks to `jobs channel and then
+	// close `jobs channel to signify no more work,
+	// that will cause the for loop in `worker to drop out
 	for _, s := range sl {
 		jobs <- s
 	}
 	close(jobs)
-	fmt.Println("**** JOBS closed ****")
+	fmt.Println("*** JOBS closed ***")
 
 	// use `results channel to gather return values
 	var r = make([]string, len(sl))
 	for i := 0; i < len(r); i++ {
-		r[i] = <-results
+		r[i] = <-results // channel recv
 	}
 	fmt.Println("\nResults:", len(r))
+	//fmt.Println("\nResults:", r)
 }
